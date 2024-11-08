a028c692df38d5c7696caa5ac196189159f7c56d
marin0suke <monk.marina@gmail.com>
2024-11-08 13:30:48 +1000

    File setup

---
+++ src/image-carousel.js
@@ -0,0 +1,8 @@
+
+function createImageCarousel() {
+    // grab the interactive elements needed. arrows
+    //dots at bottom to choose to move across.
+
+}
+
+export default createImageCarousel;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles.css";
+import createImageCarousel from "./image-carousel";
+
+//logic to display all carousels on a page goes here.
