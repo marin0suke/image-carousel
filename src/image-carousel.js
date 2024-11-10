1b619f9d4f5bfa3983c43a98eea4053b1a839d9e
marin0suke <monk.marina@gmail.com>
2024-11-10 15:58:26 +1000

    Add hardcoded HTML.

--- src/image-carousel.js
+++ src/image-carousel.js
@@ -1,7 +1,31 @@
 
 function createImageCarousel() {
-    // grab the interactive elements needed. arrows
+    // goal - show one image at a time. can sroll back and forth to change image, and skip straight to an image.
+    // grab the interactive elements needed. 
+    //outer div - the window.
+    //connect inner div with array of images.. ? and save in var.
+    // left and right arrow buttons
     //dots at bottom to choose to move across.
+
+    // function prevSlide() {
+    // }
+
+    // function nextSlide() {
+
+    // }
+
+    // function dotScroll() {
+
+    // }
+
+    // then set up event listeners.
+    //left scroll button and on click, call scroll left.
+    // same for scroll right.
+
+    
+
+
+
 
 }
 
