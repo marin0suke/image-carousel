748e4f38f0b068eeabb128bad2c09f754d1c4802
marin0suke <monk.marina@gmail.com>
2024-11-11 15:38:12 +1000

    Fix jump to slide functionality. Add styling for nav dot and its container.

--- src/image-carousel.js
+++ src/image-carousel.js
@@ -3,10 +3,26 @@ function createImageCarousel() {
     let slides = document.querySelectorAll(".slide");
     let currentSlideIndex = 0; //
 
+    const dotContainer = document.querySelector(".dot-container");
+    slides.forEach((slide, index) => {
+        const navDot = document.createElement("div");
+        navDot.classList.add("nav-dot");
+        navDot.addEventListener("click", () => jumpToSlide(index));
+        dotContainer.appendChild(navDot);
+    });
+
+    function updateActiveDot() {
+        const dots = dotContainer.querySelectorAll(".nav-dot");
+        dots.forEach((dot, index) => {
+            dot.classList.toggle("active", index === currentSlideIndex);
+        })
+    }
+
     function trackCurrentSlide() {
         slides.forEach((slide, index) => {
             slide.style.display = index === currentSlideIndex ? "block" : "none";
         });
+        updateActiveDot();
     }
 
     function prevSlide() {
@@ -24,14 +40,12 @@ function createImageCarousel() {
         trackCurrentSlide();
     }
 
+
     // then set up event listeners.
-    //left scroll button and on click, call scroll left.
-    // same for scroll right.
+    document.querySelector(".prev-arrow").addEventListener("click", prevSlide);
+    document.querySelector(".next-arrow").addEventListener("click", nextSlide); 
 
-    
-
-
-
+    trackCurrentSlide(); // initilise carousel.
 
 }
 
--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import "./styles.css";
 import createImageCarousel from "./image-carousel";
 
 //logic to display all carousels on a page goes here.
+
+document.addEventListener("DOMContentLoaded", () => {
+    createImageCarousel();
+});
+
